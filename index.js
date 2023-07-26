f9624bac42b327ba55f724dbb11cb5fa8e882f17
Adrienne Kecskes <akecs619@yahoo.com>
2023-07-26 17:21:54 -0500

    add switch

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const connection = mysql.createConnection(
 );
 
 function init() {
-  inquirer. prompt(questions).then((answers) => {
-    console.log(JSON.stringify(answers));
-    connection.end()
+  inquirer.prompt(questions).then((answers) => {
+    userResponse(answers);
+    connection.end();
   });
 }
 const questions = [
@@ -22,16 +22,44 @@ const questions = [
     name: "userChoice",
     message: "What would you like to do?",
     type: "list",
-    choices: [      
-    { name: "view all departments", value: "ALL_DEPARTMENTS" },
-    { name: "view all roles", value: "ALL_ROLES" },
-    { name: "view all employees", value: "ALL_EMPLOYEES" },
-    { name: "add a department", value: "ADD_DEPARTMENTS" },
-    { name: "add a role", value: "ADD_ROLES" },
-    { name: "add an employee", value: "ADD_EMPLOYEES" },
-    { name: "update an employee role", value: "UPDATE_EMPLOYEES" },
-],
+    choices: [
+      { name: "view all departments", value: "ALL_DEPARTMENTS" },
+      { name: "view all roles", value: "ALL_ROLES" },
+      { name: "view all employees", value: "ALL_EMPLOYEES" },
+      { name: "add a department", value: "ADD_DEPARTMENTS" },
+      { name: "add a role", value: "ADD_ROLES" },
+      { name: "add an employee", value: "ADD_EMPLOYEES" },
+      { name: "update an employee role", value: "UPDATE_EMPLOYEES" },
+    ],
   },
 ];
+
+function userResponse(answers) {
+  const { userChoice } = answers;
+  console.log(JSON.stringify(userChoice));
+  console.log(answers);
+
+  switch (userChoice) {
+    case "ALL_DEPARTMENTS":
+      console.log("here is all depts");
+
+      break;
+
+    case "ALL_ROLES":
+      break;
+
+    case "ALL_EMPLOYEES":
+      break;
+    case "ADD_DEPARTMENTS":
+      break;
+    case "ADD_ROLES":
+      break;
+    case "UPDATE_EMPLOYEES":
+      break;
+
+    default:
+      break;
+  }
+}
 
 init();
