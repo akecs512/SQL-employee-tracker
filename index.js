aede9fbc157366dbd31a834fd7847ce15797bfed
Adrienne Kecskes <akecs619@yahoo.com>
2023-07-26 16:59:29 -0500

    fix prompts

--- index.js
+++ index.js
@@ -12,21 +12,26 @@ const connection = mysql.createConnection(
 );
 
 function init() {
-  const prompt = inquirer.createPromptModule();
-  prompt({
-    type: "list",
-    name: "choices",
-    message: "What would you like to do?",
-    choices: [
-      { name: "view all departments", value: "ALL_DEPARTMENTS" },
-      { name: "view all roles", value: "ALL_ROLES" },
-      { name: "view all employees", value: "ALL_EMPLOYEES" },
-      { name: "add a department", value: "ADD_DEPARTMENTS" },
-      { name: "add a role", value: "ADD_ROLES" },
-      { name: "add an employee", value: "ADD_EMPLOYEES" },
-      { name: "update an employee role", value: "UPDATE_EMPLOYEES" },
-    ],
+  inquirer. prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers));
+    connection.end()
   });
 }
+const questions = [
+  {
+    name: "userChoice",
+    message: "What would you like to do?",
+    type: "list",
+    choices: [      
+    { name: "view all departments", value: "ALL_DEPARTMENTS" },
+    { name: "view all roles", value: "ALL_ROLES" },
+    { name: "view all employees", value: "ALL_EMPLOYEES" },
+    { name: "add a department", value: "ADD_DEPARTMENTS" },
+    { name: "add a role", value: "ADD_ROLES" },
+    { name: "add an employee", value: "ADD_EMPLOYEES" },
+    { name: "update an employee role", value: "UPDATE_EMPLOYEES" },
+],
+  },
+];
 
 init();
