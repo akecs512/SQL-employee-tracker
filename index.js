a01410d4e43ad825a288ec812227f6401914d0b8
Adrienne Kecskes <akecs619@yahoo.com>
2023-07-26 16:12:24 -0500

    fix prompts

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mysql = require(`mysql2`);
 const inquirer = require(`inquirer`);
 
-const db = mysql.connection(
+const connection = mysql.createConnection(
   {
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "",
     database: "employee_db",
@@ -12,23 +12,21 @@ const db = mysql.connection(
 );
 
 function init() {
-  inquirer.createPromptModule([
-    {
-      type: "list",
-      name: "choices",
-      message: "What would you like to do?",
-      choices: [
-        { choice: "view all departments", value: "ALL_DEPARTMENTS" },
-        { choice: "view all roles", value: "ALL_ROLES" },
-        { choice: "view all employees", value: "ALL_EMPLOYEES" },
-        { choice: "add a department", value: "ADD_DEPARTMENTS" },
-        { choice: "add a role", value: "ADD_ROLES" },
-        { choice: "add an employee", value: "ADD_EMPLOYEES" },
-        { choice: "update an employee role", value: "UPDATE_EMPLOYEES" },
-      ],
-    },
-  ]);
+  const prompt = inquirer.createPromptModule();
+  prompt({
+    type: "list",
+    name: "choices",
+    message: "What would you like to do?",
+    choices: [
+      { choice: "view all departments", value: "ALL_DEPARTMENTS" },
+      { choice: "view all roles", value: "ALL_ROLES" },
+      { choice: "view all employees", value: "ALL_EMPLOYEES" },
+      { choice: "add a department", value: "ADD_DEPARTMENTS" },
+      { choice: "add a role", value: "ADD_ROLES" },
+      { choice: "add an employee", value: "ADD_EMPLOYEES" },
+      { choice: "update an employee role", value: "UPDATE_EMPLOYEES" },
+    ],
+  });
 }
-
 
 init();
