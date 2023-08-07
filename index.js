190f60a4c032efd096774f9b2cbc55823ecd0369
Adam Kecskes <adam@kecskes.net>
2023-08-06 19:42:57 -0500

    make the promptquery generic and handle default swtich

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require(`mysql2`);
 const inquirer = require(`inquirer`);
-const { printTable } = require('console-table-printer');
+const { printTable } = require("console-table-printer");
 
 const connection = mysql.createConnection(
   {
@@ -35,31 +35,33 @@ const questions = [
 ];
 
 const errorOrDisplay = (err, list) => {
-  if (err) { console.error(err)} else { printTable(list)}
-}
-const displayTable = (list) => {
-  console.table(list)
-}
+  if (err) {
+    console.error(err);
+  } else {
+    printTable(list);
+  }
+};
+
 async function userResponse(answers) {
   const { userChoice } = answers;
   switch (userChoice) {
     case "ALL_DEPARTMENTS":
       connection.query("SELECT * FROM departments", (err, results, _fields) => {
-        errorOrDisplay(err, results)
+        errorOrDisplay(err, results);
       });
       connection.end();
       break;
 
     case "ALL_ROLES":
       connection.query("SELECT * FROM roles", (err, results, _fields) => {
-        errorOrDisplay(err, results)
+        errorOrDisplay(err, results);
       });
       connection.end();
       break;
 
     case "ALL_EMPLOYEES":
       connection.query("SELECT * FROM employees", (err, results, _fields) => {
-        errorOrDisplay(err, results)
+        errorOrDisplay(err, results);
       });
       connection.end();
       break;
@@ -69,36 +71,47 @@ async function userResponse(answers) {
         (err, results, _fields) => {}
       );
       break;
+
     case "ADD_ROLES":
       const roleQuestions = [
         { name: "title", type: "input", message: "title?" },
         { name: "salary", type: "input", message: "salary?" },
         { name: "departmentId", type: "input", message: "department id?" },
+      
       ];
-      addRoleQuery(roleQuestions);
 
+      const query = (answers) => {
+        const { title, salary, departmentId } = answers;
+        return `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${departmentId}')`;
+      };
+
+      promptAndQuery(roleQuestions, query);
       break;
+
     case "UPDATE_EMPLOYEES":
       break;
 
     default:
-      break;
+      console.error("Failed to handle a question of type:", userChoice)
+      process.exit(0)
+      // break; // unnecessary with exit
   }
 }
 
-addRoleQuery = (questions) => {
+/**
+ * Generic inquiry prompt handler
+ * @param questions is the list of question objects to be asked
+ * @param sqlQuery is a template string _function_ that destructures the answers into a proper SQL query.
+ */
+promptAndQuery = (questions, sqlQuery) => {
   inquirer.prompt(questions).then((answers) => {
-    const {title, salary, departmentId} = answers
-    connection.query(
-      `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${departmentId}')`,
-      (err, results, _fields) => {
-        if (err) {
-          console.error(error)
-        } else {
-          console.log(`Inserted new role with id of: ${results.insertId}`)
-        }
+    connection.query(sqlQuery(answers), (err, results, _fields) => {
+      if (err) {
+        console.error(error);
+      } else {
+        console.log(`Inserted new role with id of: ${results.insertId}`);
       }
-    );
+    });
     connection.end();
   });
 };
