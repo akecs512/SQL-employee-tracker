5f3f5d80920996c54d46c1bdc54c9e995b318b58
Adrienne Kecskes <akecs619@yahoo.com>
2023-08-19 15:21:35 -0500

    fix add role

--- index.js
+++ index.js
@@ -65,36 +65,112 @@ async function userResponse(answers) {
       });
       connection.end();
       break;
+
     case "ADD_DEPARTMENTS":
-      connection.query(
-        `INSERT INTO departments (id, name) VALUES ('')`,
-        (err, results, _fields) => {}
-      );
+      const departmentsQuestions = [
+        {
+          name: "departmentName",
+          type: "input",
+          message: "What is the department name?",
+        },
+      ];
+      const query = (answers) => {
+        const { departmentName } = answers;
+        return `INSERT INTO departments (name) VALUES ('${departmentName}')`;
+      };
+      promptAndQuery(departmentsQuestions, query);
       break;
 
     case "ADD_ROLES":
-      const roleQuestions = [
-        { name: "title", type: "input", message: "title?" },
-        { name: "salary", type: "input", message: "salary?" },
-        { name: "departmentId", type: "input", message: "department id?" },
-      
+      connection.query("SELECT * FROM departments", (_err, results, _fields) => {
+        const departmentChoices = results.map((department) => {
+          return department.name;
+        });
+
+        const roleQuestions = [
+          {
+            name: "title",
+            type: "input",
+            message: "What is the title for the role?",
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this role?",
+          },
+          {
+            name: "department",
+            type: "list",
+            message: "what department does this role belong to?",
+            choices: departmentChoices,
+          },
+        ];
+
+        inquirer.prompt(roleQuestions).then((answers) => {
+          const { title, salary, department: selectedDepartment } = answers;
+          console.log('this department', selectedDepartment)
+          
+          const departmentId = results.find(
+            (d) => {
+              return d.name === selectedDepartment
+            }).id
+            
+          connection.query(
+            `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}','${departmentId}')`,
+            (err, results, _fields) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log(
+                  `Inserted new role with id of: ${results.insertId}`
+                );
+              }
+            }
+          );
+          connection.end();
+        });
+      });
+
+      break;
+
+    case "ADD_EMPLOYEES":
+      const employeeQuestions = [
+        {
+          name: "firstName",
+          type: "input",
+          message: "What is the employee's first name?",
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "What is the employee's last name?",
+        },
+        { name: "role", type: "list", message: "What is the employee's role?" },
       ];
 
-      const query = (answers) => {
-        const { title, salary, departmentId } = answers;
-        return `INSERT INTO roles (title, salary, department_id) VALUES ('${title}', '${salary}', '${departmentId}')`;
+      const query2 = (answers) => {
+        const { firstName, lastName, role } = answers;
+        return `INSERT INTO roles (title, salary, department_id) VALUES ('${firstName}', '${lastName}', '${role}')`;
       };
 
-      promptAndQuery(roleQuestions, query);
+      promptAndQuery(employeeQuestions, query2);
       break;
 
-    case "UPDATE_EMPLOYEES":
-      break;
+    // case "UPDATE_EMPLOYEES":
+    //   const updateEmployeeQuestions = [
+    //     {name: "employeeName", type: "list", message: "Which employee would you like to update?" },
+    //     {name: "employeeRole", type: "list", message: "What is the employee's new role?" },
+    //   ];
+    //   const query = (answers) => {
+    //     const { employeeName, employeeRole } = answers;
+    //     return `UPDATE employees SET role_id = ${employeeRole} WHERE id = ${employeeName}`;
+    //   };
+    //  break;
 
     default:
-      console.error("Failed to handle a question of type:", userChoice)
-      process.exit(0)
-      // break; // unnecessary with exit
+      console.error("Failed to handle a question of type:", userChoice);
+      process.exit(0);
+    // break; // unnecessary with exit
   }
 }
 
@@ -107,7 +183,7 @@ promptAndQuery = (questions, sqlQuery) => {
   inquirer.prompt(questions).then((answers) => {
     connection.query(sqlQuery(answers), (err, results, _fields) => {
       if (err) {
-        console.error(error);
+        console.error(err);
       } else {
         console.log(`Inserted new role with id of: ${results.insertId}`);
       }
